2a83ca92f3e764f3a2dbd2c2ae2504856d8e9cd2
khalid <khalid.ibr212@gmail.com>
2024-02-17 23:52:54 +0300

    loglib

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import CountDown from "./components/CountDown";
 import DailyQuote from "./components/DailyQuote";
+import Loglib from "@loglib/tracker/react";
 
 function App() {
   return (
     <div className="App">
+      <Loglib
+        config={{
+          id: "ramadan-countdown",
+        }}
+      />
       <CountDown />
       <DailyQuote />
     </div>
