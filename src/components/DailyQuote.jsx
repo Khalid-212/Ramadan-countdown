0c30d4b1ffc9c501dee30409d4c7e54220fad46c
khalid <khalid.ibr212@gmail.com>
2024-02-17 21:22:19 +0300

    daily qoutes page

---
+++ src/components/DailyQuote.jsx
@@ -0,0 +1,27 @@
+function DailyQuote() {
+  const quotes = [
+    "The key to success is reflected in the Qur'an.",
+    "Don't be a servant of Ramadan, be a servant of Allah.",
+    "May Allah shower his blessings upon you in this holy month.",
+  ];
+
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+  return (
+    <div className="DailyQuote">
+      <div className="quote">
+        <p>{randomQuote}</p>
+      </div>
+      <footer className="footer">
+        <p>
+          Made with <span>❤</span> by{" "}
+          <a href="https://khalidibrahim.co/" target="blank">
+            Khalid Ibrahim
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default DailyQuote;
